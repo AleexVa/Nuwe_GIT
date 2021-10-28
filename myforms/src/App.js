8b39886c199fa20e977035e2be49a05d820ad16b
Alexander <alexandervalalm@monlau.com>
2021-10-28 11:38:37 +0200

    Añadir myforms final

--- myforms/src/App.js
+++ myforms/src/App.js
@@ -27,24 +27,31 @@ const CustomButton1 = withStyles({
   },
 })(props => <Button {...props} />);
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  Swal.fire({title: 'COOL!', 
+             text: 'FORM HAS BEEN SENT SUCCESFULLY',   
+             icon: 'success',        
+             confirmButtonText: 'OKEY!'});
+}
 
 function App() {
   const [username, setUsername] = React.useState("")
   const [password, setPassword] = React.useState("")
   return (
-    <div className="App"> 
-        <form className="App-header" onSubmit={()=>Swal.fire("Username: " + username + "\n" + "Password: " + password)}>
-          <Grid container justify="center" alignItems="center" ml={20}>
-            <Grid item xs={6} style={{ textAlign: 'left' }}>
-              <h1 className="header-1">FORMS <br /> WITH <br /> REACT</h1>
-            </Grid>
-            <Box sx={{ display: 'grid', gridTemplateRows: 'repeat(3, 1fr)', gridGap: '30px' }}>
-              <TextField className="t1" id="outlined-basic" value={username} onChange={(e)=>setUsername(e.target.value)} type="text" label="USER" variant="outlined" required />
-              <TextField className="t1" id="outlined-basic" value={password} onChange={(e)=>setPassword(e.target.value)} type="password" label="PASSWORD" variant="outlined" required />
-              <CustomButton1 variant="outlined" type="submit">SUBMIT</CustomButton1>
-            </Box>
+    <div className="App">
+      <form className="App-header" onSubmit={handleSubmit}>
+        <Grid container justify="center" alignItems="center" ml={20}>
+          <Grid item xs={6} style={{ textAlign: 'left' }}>
+            <h1 className="header-1">FORMS <br /> WITH <br /> REACT</h1>
           </Grid>
-        </form>
+          <Box sx={{ display: 'grid', gridTemplateRows: 'repeat(3, 1fr)', gridGap: '30px' }}>
+            <TextField className="t1" id="outlined-basic" value={username} onChange={(e) => setUsername(e.target.value)} type="text" label="USER" variant="outlined" required />
+            <TextField className="t1" id="outlined-basic" value={password} onChange={(e) => setPassword(e.target.value)} type="password" label="PASSWORD" variant="outlined" required />
+            <CustomButton1 variant="outlined" type="submit">SUBMIT</CustomButton1>
+          </Box>
+        </Grid>
+      </form>
     </div>
   );
 }
